f1fdaf3027155929ef35ea3c5af30de507c13899
Henry Fellerhoff <henryfellerhoff@gmail.com>
2020-02-16 11:11:18 +0100

    Project screen button animations

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 import Layout from '../../components/Layout';
 import ProjectRoll from '../../components/ProjectRoll';
 import classes from './projects.module.scss';
+import Helmet from 'react-helmet';
 
 const ProjectPage = () => {
   return (
     <Layout>
+      <Helmet titleTemplate='%s | Henry Fellerhoff'>
+        <title>Projects</title>
+      </Helmet>
       <div className={classes.container}>
         <h1 className={classes.pageTitle}>Projects</h1>
         <ProjectRoll />
