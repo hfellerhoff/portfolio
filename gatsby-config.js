615599d708e53185c29d7970760ac57b705d41fb
Henry Fellerhoff <henryfellerhoff@gmail.com>
2020-02-28 17:19:15 +0100

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,23 @@ module.exports = {
       'Passionate software engineer and web developer who loves building impactful things.',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-159295601-1',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
